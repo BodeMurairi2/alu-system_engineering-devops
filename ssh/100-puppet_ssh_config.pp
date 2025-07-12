fad9e90d991da83c9cb5a26736f787afc2573e8b
BodeMurairi2 <b.murairi@alustudent.com>
2025-07-12 19:30:54 +0200

    Task 100

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,10 @@
-# Puppet manifest to configure SSH client for key-based authentication and disable password auth
-file { '~/.ssh/config':
+# Puppet manifest to configure system-wide SSH client for key-based auth and disable password authentication
+
+file { '/etc/ssh/ssh_config':
   ensure  => file,
-  owner   => 'bodemurairi',
-  group   => 'bodemurairi',
-  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
   content => @("CONFIG"/L),
     Host *
         IdentityFile ~/.ssh/school
